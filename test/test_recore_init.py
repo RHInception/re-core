d919d1fc142b3484d7574c3e3363dda8ae7a32d6
Tim Bielawa <tbielawa@redhat.com>
2014-04-21 11:56:40 -0400

    Testing reading in config files and logging initialization

--- test/test_recore_init.py
+++ test/test_recore_init.py
@@ -14,9 +14,42 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from . import TestCase, unittest
-from recore import start_logging
+import recore
 import logging
+import mock
 
-class TestUtils(TestCase):
+class TestRecoreInit(TestCase):
+    def setUp(self):
+        self.config_file_dne = '/dev/ihopethisfiledoesntexist.json'
+        self.config_file_invalid = './test/files/settings-example-invalid.json'
+        self.config_file_valid = './test/files/settings-example.json'
+        self.log_level = logging.INFO
+        self.log_level_stdout = logging.DEBUG
+
+
     def test_start_logging(self):
-        start_logging('/dev/null', 'DEBUG')
+        """Loggers are created with appropriate handlers associated"""
+        recore.start_logging('/dev/null', self.log_level)
+        _logcore = logging.getLogger('recore')
+        _logcorestdout = logging.getLogger('recore.stdout')
+        assert len(_logcore.handlers) == 1
+        assert len(_logcorestdout.handlers) == 1
+        self.assertEqual(_logcore.level, self.log_level,
+                         msg="logcore level is actually %s but we wanted %s" % (_logcore.level, self.log_level))
+        self.assertEqual(_logcorestdout.level, self.log_level_stdout,
+                         msg="logcorestdout level is actually %s but we wanted %s" % (_logcorestdout.level, self.log_level_stdout))
+
+
+    def test_parse_config(self):
+        """An example configuration file can be parsed"""
+        # Case 1: File does not exist
+        with mock.patch('recore.start_logging'):
+            with self.assertRaises(SystemExit):
+                cfg = recore.parse_config(self.config_file_dne)
+        # Case 2: File is not valid json
+        with mock.patch('recore.start_logging'):
+            with self.assertRaises(SystemExit):
+                cfg = recore.parse_config(self.config_file_invalid)
+        # Case 3: File exists and is valid json
+        with mock.patch('recore.start_logging'):
+            cfg = recore.parse_config(self.config_file_valid)
