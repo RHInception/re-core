901e446ac0d461964909e23d7170b9a8a93d91bd
Steve Milner <stevem@gnulinux.net>
2014-04-21 12:55:22 -0400

    Added tests for recore.mongo.

--- test/test_mongo.py
+++ test/test_mongo.py
@@ -13,6 +13,9 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import bson
+import pymongo
+import datetime
 import mock
 
 from . import TestCase, unittest
@@ -21,6 +24,7 @@ from recore import mongo
 
 
 # Mocks
+UTCNOW = datetime.datetime.utcnow()
 connection = mock.MagicMock('connection')
 
 
@@ -30,7 +34,6 @@ class TestMongo(TestCase):
         """
         Reset mocks.
         """
-#        mongo.MongoClient.reset_mock()
         connection.reset_mock()
 
     def test_connect(self):
@@ -71,3 +74,113 @@ class TestMongo(TestCase):
 
         # Error result is {}
         assert mongo.lookup_project({}, "project") == {}
+
+    def test_initialize_state(self):
+        """
+        Make sure that creating the initial state uses proper data
+        """
+        db = mock.MagicMock()
+        collection = mock.MagicMock()
+        collection.insert = mock.MagicMock(return_value=12345)
+        db.__getitem__.return_value = collection
+        project = 'testproject'
+
+        with mock.patch('recore.mongo.datetime.datetime') as (
+                mongo.datetime.datetime):
+            mongo.datetime.datetime = mock.MagicMock('datetime')
+            mongo.datetime.datetime.utcnow = mock.MagicMock(
+                return_value=UTCNOW)
+
+            mongo.initialize_state(db, project)
+            db['state'].insert.assert_called_once_with({
+                'project': project,
+                'step_log': [],
+                'created': UTCNOW,
+                'running': True,
+            })
+
+    def test_initialize_state_with_error(self):
+        """
+        Make sure that if mongo errors out we are notified with the
+        proper exception
+        """
+        db = mock.MagicMock()
+        collection = mock.MagicMock()
+        collection.insert = mock.MagicMock(
+            side_effect=pymongo.errors.PyMongoError('test error'))
+        db.__getitem__.return_value = collection
+        project = 'testproject'
+
+        # We should get a PyMongoError
+        self.assertRaises(
+            pymongo.errors.PyMongoError, mongo.initialize_state, db, project)
+
+    def test_mark_release_running(self):
+        """
+        Verify that mark_release_running inserts the proper information
+        """
+        db = mock.MagicMock()
+        collection = mock.MagicMock()
+        collection.update = mock.MagicMock(mock.MagicMock(return_value=12345))
+        db.__getitem__.return_value = collection
+        objectid = bson.ObjectId()
+
+        for status in (True, False):
+            mongo.mark_release_running(db, objectid, status)
+
+            db['state'].update.assert_called_once_with(
+                {'_id': objectid},
+                {'$set': {'running': status}})
+            db['state'].update.reset_mock()
+
+    def test_mark_release_running_with_error(self):
+        """
+        Make sure that if mongo errors out while marking a release
+        we are notified with the proper exception
+        """
+        db = mock.MagicMock()
+        collection = mock.MagicMock()
+        collection.update = mock.MagicMock(
+            side_effect=pymongo.errors.PyMongoError('test error'))
+        db.__getitem__.return_value = collection
+        objectid = bson.ObjectId()
+
+        # We should get a PyMongoError
+        self.assertRaises(
+            pymongo.errors.PyMongoError,
+            mongo.mark_release_running, db, objectid, True)
+
+    def test_update_state(self):
+        """
+        Verify that update_state inserts the proper information
+        """
+        db = mock.MagicMock()
+        collection = mock.MagicMock()
+        collection.update = mock.MagicMock(mock.MagicMock(return_value=12345))
+        db.__getitem__.return_value = collection
+        objectid = bson.ObjectId()
+
+        for state in ('completed', 'failed'):
+            mongo.update_state(db, objectid, state)
+
+            db['state'].update.assert_called_once_with(
+                {'_id': objectid},
+                {'$push': {'step_log': state}})
+            db['state'].update.reset_mock()
+
+    def test_update_state_with_error(self):
+        """
+        Make sure that if mongo errors out while updating a state
+        we are notified with the proper exception
+        """
+        db = mock.MagicMock()
+        collection = mock.MagicMock()
+        collection.update = mock.MagicMock(
+            side_effect=pymongo.errors.PyMongoError('test error'))
+        db.__getitem__.return_value = collection
+        objectid = bson.ObjectId()
+
+        # We should get a PyMongoError
+        self.assertRaises(
+            pymongo.errors.PyMongoError,
+            mongo.update_state, db, objectid, 'completed')
